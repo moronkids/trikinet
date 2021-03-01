4a5c26ade30cbe613224022758d4e8f929930e1b
Sulistyo Ardani <sulistyoardani@gmail.com>
2021-03-01 15:54:45 +0700

    SULIS - boilerplate nextjs

---
+++ pages/_app.js
@@ -0,0 +1,6 @@
+// import "../styles.css";
+import "../styles/scss/styles.scss";
+
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
---
+++ pages/index.js
@@ -0,0 +1,7 @@
+const Index = () => (
+  <div>
+    <h1 className="h4">Home page</h1>
+  </div>
+);
+
+export default Index;
