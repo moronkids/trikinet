34e83457791eadb90e8aad89d95ed34372b229af
Sulistyo Ardani <sulistyoardani@gmail.com>
2021-03-02 12:43:40 +0700

    sulis -netlify

--- out/_next/static/s9PsTp7MwGUVTMxXDQZFn/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-631ac96347aad19c226f.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-d27ffe7c6180e8bbe00b.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
