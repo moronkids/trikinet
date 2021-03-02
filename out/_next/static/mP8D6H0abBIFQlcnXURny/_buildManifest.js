8435254233a7a56b0b3c105b8c0c13d6371328e0
Sulistyo Ardani <sulistyoardani@gmail.com>
2021-03-02 13:08:21 +0700

    sulis - using npm export

---
+++ out/_next/static/mP8D6H0abBIFQlcnXURny/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-24485823fd44fcfd3be8.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-902bf30cd02a0dcb8947.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
