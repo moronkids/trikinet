6e5ed7be56c986a5c03d161842636fbb8e74be70
moronkids <sulistyoardani@gmail.com>
2021-03-08 15:08:10 +0700

    sulis - adding meta description

--- pages/page/[slug].js
+++ pages/page/[slug].js
@@ -1,22 +1,23 @@
 import SearchContainers from "components/homepage/searchContainers";
 import Articlex from "components/article";
 import { useSelector } from "react-redux";
-import Head from "next/head"
+import Head from "next/head";
 const Article = () => {
   const { detailNews, latestNews } = useSelector((state) => ({
     detailNews: state.news.detailNews.data,
     latestNews: state.news.newsLatest.data,
   }));
-  console.log(detailNews, "prep")
+  console.log(detailNews, "prep");
   return (
     <>
-      {detailNews.yoast_meta && detailNews.yoast_meta.map((val, i) => {
-        return (
-          <Head>
-            <meta name={val.name} content={val.content} />
-          </Head>
-        );
-      })}
+      <Head>
+        {detailNews.yoast_meta &&
+          detailNews.yoast_meta.map((val, i) => {
+            return <meta name={val.name} content={val.content} />;
+          })}
+        <meta name="description" content={detailNews.yoast_title}></meta>
+        <title>{detailNews.yoast_title}</title>
+      </Head>
       <SearchContainers />
       <Articlex />
     </>
