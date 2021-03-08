aea013da350b37d2f5c3ebada5870adc3fa96aaa
moronkids <sulistyoardani@gmail.com>
2021-03-08 14:58:56 +0700

    sulis - adding meta in detail article

--- pages/index.js
+++ pages/index.js
@@ -2,26 +2,29 @@ import SearchContainers from "components/homepage/searchContainers";
 import SectionNews from "components/homepage/newsSection";
 // import SectionTipsTrik from "components/homepage/tipsTrikSection";
 import Head from "next/head";
+
 const Index = () => {
+
   return (
-  <>
-    <Head>
-      <meta
-        name="description"
-        content="Mary's simple recipe for maple bacon donuts
+    <>
+      <Head>
+        <meta
+          name="description"
+          content="Mary's simple recipe for maple bacon donuts
            makes a sticky, sweet treat with just a hint
            of salt that you'll keep coming back for."
-      ></meta>
-      <title>Home</title>
-      <meta property="og:title" content="My page title" key="title" />
-    </Head>
-    <Head>
-      <meta property="og:title" content="My new title" key="title" />
-    </Head>
-    <SearchContainers />
-    <SectionNews />
-    {/* <SectionTipsTrik/> */}
-  </>
-);}
+        ></meta>
+        <title>Home</title>
+        <meta property="og:title" content="My page title" key="title" />
+      </Head>
+      <Head>
+        <meta property="og:title" content="My new title" key="title" />
+      </Head>
+      <SearchContainers />
+      <SectionNews />
+      {/* <SectionTipsTrik/> */}
+    </>
+  );
+};
 
 export default Index;
--- pages/page/[slug].js
+++ pages/page/[slug].js
@@ -1,10 +1,24 @@
 import SearchContainers from "components/homepage/searchContainers";
-import Articlex from 'components/article'
+import Articlex from "components/article";
+import { useSelector } from "react-redux";
+import Head from "next/head"
 const Article = () => {
+  const { detailNews, latestNews } = useSelector((state) => ({
+    detailNews: state.news.detailNews.data,
+    latestNews: state.news.newsLatest.data,
+  }));
+  console.log(detailNews, "prep")
   return (
     <>
+      {detailNews.yoast_meta && detailNews.yoast_meta.map((val, i) => {
+        return (
+          <Head>
+            <meta name={val.name} content={val.content} />
+          </Head>
+        );
+      })}
       <SearchContainers />
-      <Articlex/>
+      <Articlex />
     </>
   );
 };
