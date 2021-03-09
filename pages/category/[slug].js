33e30131df74708c56cb09d08e61fc6f258e97a9
moronkids <sulistyoardani@gmail.com>
2021-03-09 16:21:06 +0700

    sulis - adding head for seo in page category

--- pages/category/[slug].js
+++ pages/category/[slug].js
@@ -4,6 +4,21 @@ import SearchContainers from "components/layouts/searchContainers";
 const Web = () => {
   return (
     <>
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#90cdf4" />
+        <meta
+          name="description"
+          content="Mary's simple recipe for maple bacon donuts
+           makes a sticky, sweet treat with just a hint
+           of salt that you'll keep coming back for."
+        ></meta>
+        <title>Home</title>
+        <meta property="og:title" content="My page title" key="title" />
+      </Head>
+      <Head>
+        <meta property="og:title" content="My new title" key="title" />
+      </Head>
       <SearchContainers />
       <Web_ />
     </>
