cf5effe67cc7a6ce48b201e5670d9ca7c2d08a23
Sulistyo Ardani <sulistyoardani@gmail.com>
2021-03-16 18:26:32 +0700

    sulis - css purge

--- pages/search/[slug].js
+++ pages/search/[slug].js
@@ -1,4 +1,4 @@
-import React, {  } from "react";
+import React, { useState } from "react";
 import dynamic from "next/dynamic";
 import { useRouter } from "next/router";
 import { useSelector, useDispatch } from "react-redux";
