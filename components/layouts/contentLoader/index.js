21c0fe77b310328bfae1d7474fefb63b237bd3bf
Sulistyo Ardani <sulistyoardani@gmail.com>
2021-03-18 11:13:49 +0700

    sulis - setting loader width 100%

--- components/layouts/contentLoader/index.js
+++ components/layouts/contentLoader/index.js
@@ -4,14 +4,14 @@ import ContentLoader from "react-content-loader";
 const MyLoader = (props) => (
   <ContentLoader
     speed={1}
-    // width={500}
+    width={"100%"}
     height={400}
     // viewBox="0 0 510 400"
     backgroundColor="#f3f3f3"
     foregroundColor="#ecebeb"
     {...props}
   >
-    <rect x="3" y="0" rx="0" ry="0" width="448" height="327" />
+    <rect x="3" y="0" rx="0" ry="0" width="600" height="327" />
   </ContentLoader>
 );
 
--- components/layouts/contentLoader/loader.js
+++ components/layouts/contentLoader/loader.js
@@ -4,7 +4,7 @@ import ContentLoader from "react-content-loader";
 const MyLoader = (props) => (
   <ContentLoader
     speed={2}
-    // width={400}
+    width={"100%"}
     height={160}
     // viewBox="0 0 400 160"
     backgroundColor="#dedede"
@@ -12,12 +12,12 @@ const MyLoader = (props) => (
     {...props}
   >
     <rect x="0" y="0" rx="0" ry="0" width="120" height="120" />
-    <rect x="143" y="0" rx="0" ry="0" width="188" height="16" />
-    <rect x="143" y="20" rx="0" ry="0" width="188" height="16" />
-    <rect x="143" y="40" rx="0" ry="0" width="188" height="16" />
-    <rect x="143" y="60" rx="0" ry="0" width="188" height="16" />
-    <rect x="143" y="80" rx="0" ry="0" width="188" height="16" />
-    <rect x="143" y="100" rx="0" ry="0" width="188" height="16" />
+    <rect x="143" y="0" rx="0" ry="0" width="250" height="16" />
+    <rect x="143" y="20" rx="0" ry="0" width="250" height="16" />
+    <rect x="143" y="40" rx="0" ry="0" width="250" height="16" />
+    <rect x="143" y="60" rx="0" ry="0" width="250" height="16" />
+    <rect x="143" y="80" rx="0" ry="0" width="250" height="16" />
+    <rect x="143" y="100" rx="0" ry="0" width="250" height="16" />
   </ContentLoader>
 );
 
