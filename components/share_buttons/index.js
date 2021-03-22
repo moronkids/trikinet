f6eaacdc5b2df6bcc5bf53e40151cc285312c60c
Sulistyo Ardani <sulistyoardani@gmail.com>
2021-03-22 15:48:13 +0700

    sulis - share buttons

--- components/share_buttons/index.js
+++ components/share_buttons/index.js
@@ -1,49 +1,57 @@
-import React from 'react';
+import React from "react";
 
 const Share = (props) => {
-    return (
-      <>
-        <div className={"boxShare " + props.class}>
-          <div className="col-3 m-0 p-0 py-2 w-100 h-100">
-            <div
-              className="d-flex mx-2 style w-auto h-100 "
-              style={{ backgroundColor: "#2BCC63", borderRadius: "5px" }}
-            >
-              <div className="whatsApp my-auto ml-2 mx-auto" />
+  return (
+    <>
+      <div className={"boxShare " + props.class}>
+        <div className="col-3 m-0 p-0 py-2 w-100 h-100">
+          <div
+            className="d-flex mx-2 style w-auto h-100 "
+            style={{ backgroundColor: "#2BCC63", borderRadius: "5px" }}
+          >
+            <div className="mx-auto d-flex">
+              <div className="whatsApp my-auto" />
               <span className="my-auto pl-1 d-none d-md-block">Share</span>
             </div>
           </div>
-          <div className="col-3 my-auto w-100 h-100 m-0 p-0 py-2">
-            {" "}
-            <div
-              className="d-flex mx-2 style w-auto h-100"
-              style={{ backgroundColor: "#3A5EA6", borderRadius: "5px" }}
-            >
+        </div>
+        <div className="col-3 my-auto w-100 h-100 m-0 p-0 py-2">
+          {" "}
+          <div
+            className="d-flex mx-2 style w-auto h-100"
+            style={{ backgroundColor: "#3A5EA6", borderRadius: "5px" }}
+          >
+            <div className="mx-auto d-flex">
               <div className="fb my-auto ml-2 mx-auto" />
               <span className="my-auto pl-1 d-none d-md-block">Share</span>
             </div>
           </div>
-          <div className="col-3 m-0 p-0 py-2 w-100 h-100">
-            <div
-              className="d-flex mx-2 style w-auto h-100"
-              style={{ backgroundColor: "#1db7eb", borderRadius: "5px" }}
-            >
+        </div>
+        <div className="col-3 m-0 p-0 py-2 w-100 h-100">
+          <div
+            className="d-flex mx-2 style w-auto h-100"
+            style={{ backgroundColor: "#1db7eb", borderRadius: "5px" }}
+          >
+            <div className="mx-auto d-flex">
               <div className="twitter my-auto ml-2 mx-auto" />
               <span className="my-auto pl-1 d-none d-md-block">Tweet</span>
             </div>
           </div>
-          <div className="col-3 my-auto w-100 h-100 m-0 p-0 py-2">
-            <div
-              className="d-flex mx-2 style w-auto h-100"
-              style={{ backgroundColor: "#cc0300", borderRadius: "5px" }}
-            >
+        </div>
+        <div className="col-3 my-auto w-100 h-100 m-0 p-0 py-2">
+          <div
+            className="d-flex mx-2 style w-auto h-100"
+            style={{ backgroundColor: "#cc0300", borderRadius: "5px" }}
+          >
+            <div className="mx-auto d-flex">
               <div className="flip my-auto ml-2 mx-auto" />
               <span className="my-auto pl-1 d-none d-md-block">Flip</span>
             </div>
           </div>
         </div>
-      </>
-    );
+      </div>
+    </>
+  );
 };
 
 export default Share;
