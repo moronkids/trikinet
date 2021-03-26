c6d1d95fa1ecc3c88674a0779b3bab5d011dab5d
Sulistyo Ardani <sulistyoardani@gmail.com>
2021-03-26 16:17:08 +0700

    sulis - pwa

--- next.config.js
+++ next.config.js
@@ -1,24 +1,24 @@
-// const withPWA = require("next-pwa");
-// module.exports = withPWA({
-//   pwa: {
-//     dest: "public",
-//   },
-//   module: {
-//     rules: [
-//       {
-//         test: /\.css$/i,
-//         use: ["style-loader", "css-loader", "postcss-loader"],
-//       },
-//     ],
-//     images: {
-//       domains: ["spiderman.trikinet.com"],
-//     },
-//   },
-// });
-
-module.exports = {
-  images: {
-    domains: ["spiderman.trikinet.com"]
+const withPWA = require("next-pwa");
+module.exports = withPWA({
+  pwa: {
+    dest: "public",
   },
-};
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+    ],
+    images: {
+      domains: ["spiderman.trikinet.com"],
+    },
+  },
+});
+
+// module.exports = {
+//   images: {
+//     domains: ["spiderman.trikinet.com"]
+//   },
+// };
 
